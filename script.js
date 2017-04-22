134303f82fb1b58d6d4f6d756f852f5f6dcd44e4
Pawel Dabrowa <d@browa.co.uk>
2017-04-22 17:51:24 +0100

    added throttle to javascript

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 var article = document.querySelector('article'),
 	windowHeight = window.innerHeight,
 	articleHeight = article.clientHeight,
@@ -9,3 +11,23 @@ progress.value = 0;
 progress.max = maxScroll;
 
 article.appendChild(progress);
+
+var calculateProgress = function(){
+	progress.value = window.scrolly;
+};
+
+var throttle = function(callback, limit){
+	var wait = false;
+	return function(){
+		if(!wait){
+			callback;
+			wait = true;
+			setTimeout(function(){
+				wait = false;
+				}, limit);
+			};
+		};	
+	};
+		
+window.addEventListener('scroll', throttle(calculateProgress, 200));
+consol
